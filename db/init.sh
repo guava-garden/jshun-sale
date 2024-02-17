4a6cc32b8eb3ce5c048e6489f4247267bb05e20f
Wesley Nien <wesleynien@users.noreply.github.com>
2024-02-17 04:49:19 +0000

    - Enhancement for deployment for GCP arm64 VM   - Fix: DB_PASS not exported as env var   - Add: command cheatsheet   - Change: PV size for mysql = 1Gi   - Fix: tolerations not added for deployment in GCP arm64 VM

--- db/init.sh
+++ db/init.sh
@@ -2,6 +2,8 @@
 
 #echo "input DB_PASS: "
 read -s -p "input DB_PASS: " DB_PASS
+export DB_PASS
+
 cat <<EOF > kustomization.yaml.sample
 secretGenerator:
 - name: mysql-pass
@@ -10,12 +12,36 @@ secretGenerator:
 resources:
   - mysql-deployment.yaml
 EOF
+
 echo
-echo "--- deploy with command below ---"
+echo "------------------"
+echo "command cheatsheet"
+echo "------------------"
+echo
+
+echo "--- deploy mysql service ---"
 echo "mv kustomization.yaml kustomization.yaml.bak"
 echo "mv kustomization.yaml.sample kustomization.yaml"
 echo "kubectl apply -k ./"
 echo
-echo "--- delete with command below ---"
-echo "kubectl delete -k ./"
 
+echo "--- delete mysql service ---"
+echo "kubectl delete -k ./"
+echo
+
+echo "--- init DB with saledb-init.sql ---"
+cat <<EOF
+export POD__=\$( kubectl get pods -l "app=wordpress,tier=mysql" -o jsonpath="{.items[0].metadata.name }" )
+kubectl exec -it \$POD__ -- mysql -u root -p\$DB_PASS < saledb-init.sql
+EOF
+echo
+
+echo "--- check DB.customer table ---"
+cat <<EOF
+export POD__=\$( kubectl get pods -l "app=wordpress,tier=mysql" -o jsonpath="{.items[0].metadata.name }" )
+kubectl exec -it \$POD__ -- mysql --default-character-set=utf8 -u root -p\$DB_PASS -D saledb -e 'select * from customer;'
+EOF
+# alternative way
+# export POD_IP__=\$( kubectl get pods -l "app=wordpress,tier=mysql" -o jsonpath="{.items[0].status.podIP}" )
+# kubectl run mysqlclient --rm -it --restart=Never --image=mysql:8.0 --overrides='{"spec":{"tolerations":[{"key":"kubernetes.io/arch","value":"arm64","effect":"NoSchedule"}]}}' --command -- mysql --default-character-set=utf8 -h \$POD_IP__ -u root -p\$DB_PASS -D saledb -e 'select * from customer;'
+echo
